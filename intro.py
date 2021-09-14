4d25ed5ae0ce7dc808205d749b0ce5f617878295
Aneeq-Younas <85731812+Aneeq-Younas@users.noreply.github.com>
2021-09-13 23:13:07 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,15 @@
+
+def intro( ):
+    name= "Aneeq "
+    age= 23
+    address= "Lahore, Pakistan  "
+    occupation= "Coding & Programming "
+    print("Your name is : {}\n Your age is: {}\n Your address is: {}\n Your occupation is: {}  "
+          . format(name, age, address, occupation))
+
+intro()
+
+
+
+
+
